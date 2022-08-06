cfce8a1685c1e2fc0cfc92c05d6015064ef6e627
Shivansh Mittal <81565355+Shivansh-Mittal@users.noreply.github.com>
2022-08-06 15:17:09 +0530

    Optimized the Code

--- Functions/check-even-odd.java
+++ Functions/check-even-odd.java
@@ -1,22 +1,11 @@
 import java.util.*;
+
 class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("\n Hello, World! \n"); 
+    public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in);
         int num;
         System.out.println("Enter the number: ");
         num = sc.nextInt();
-        System.out.println(num + " is " + checkEven(num));
-    }
-    
-    static String checkEven(int n){
-        String res = "";
-        if(n%2==0){
-            res = "even";
-        }
-        else{
-            res = "odd";
-        }
-        return res;
+        System.out.println((num%2==0)?"Even":"Odd");
     }
 }
