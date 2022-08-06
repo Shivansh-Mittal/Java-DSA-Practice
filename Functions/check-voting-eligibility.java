0e745d9a8f6ce1a25f5377beab7fe609dcb92b05
Shivansh Mittal <81565355+Shivansh-Mittal@users.noreply.github.com>
2022-08-06 15:22:56 +0530

    Optimized the code by ternary operator

--- Functions/check-voting-eligibility.java
+++ Functions/check-voting-eligibility.java
@@ -6,17 +6,22 @@ class HelloWorld {
         int num;
         System.out.println("Enter your Age: ");
         num = sc.nextInt();
-        System.out.println("You are "+voting(num)+" to vote.");
+        checkEligible(num);
+        // System.out.println("You are "+voting(num)+" to vote.");
     }
     
-    static String voting(int n){
-        String res = "";
-        if(n>=18){
-            res = "eligible";
-        }
-        else{
-            res = "not eligible";
-        }
-        return res;
+    static void checkEligible(int age){
+        System.out.println((age>=18)?"Eligible to Vote":"Not Eligible to vote");
     }
+    
+//     static String voting(int n){
+//         String res = "";
+//         if(n>=18){
+//             res = "eligible";
+//         }
+//         else{
+//             res = "not eligible";
+//         }
+//         return res;
+//     }
 }
