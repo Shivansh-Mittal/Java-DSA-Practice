2fab38d1b115b6cd9dde52f56babf0a93adf6806
Shivansh Mittal <81565355+Shivansh-Mittal@users.noreply.github.com>
2022-08-06 15:34:45 +0530

    Update sum-2-numbers.java

--- Functions/sum-2-numbers.java
+++ Functions/sum-2-numbers.java
@@ -1,7 +1,6 @@
 import java.util.*;
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("\n Hello, World! \n "); 
         Scanner sc = new Scanner(System.in);
         int a,b;
         System.out.println("\n Enter the first number: \n");
