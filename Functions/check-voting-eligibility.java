2e2f18d99bf99219a64d4ed2074245870ec66a7d
Shivansh Mittal <81565355+Shivansh-Mittal@users.noreply.github.com>
2022-08-06 15:35:17 +0530

    Update check-voting-eligibility.java

--- Functions/check-voting-eligibility.java
+++ Functions/check-voting-eligibility.java
@@ -1,7 +1,6 @@
 import java.util.*;
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("\n Hello, World! \n"); 
         Scanner sc = new Scanner(System.in);
         int num;
         System.out.println("Enter your Age: ");
