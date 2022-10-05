4c3b35cdce9cf8663c68b0057604432c8d37733b
Shivansh-Mittal <shivanshmittal1444@gmail.com>
2022-10-05 19:59:29 +0530

    binarsearch folder modified so that individualfiles can be executed

---
+++ binarysearch/BinarySearch.java
@@ -0,0 +1,24 @@
+package binarysearch;
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,9,99};
+        int target = 9;
+        System.out.println(binarySearch(arr, target));
+    }
+
+    static int binarySearch(int[] nums, int target){
+        int start = 0;
+        int end = nums.length;
+
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(target < nums[mid])
+                end = mid - 1;
+            else if(target > nums[mid])
+                start = mid + 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+}
---
+++ binarysearch/Sqrtx.java
@@ -0,0 +1,31 @@
+package binarysearch;
+public class Sqrtx {
+    public static void main(String[] args) {
+        int num = 9778;
+        System.out.println(mySqrt(num));
+    }
+
+    static int mySqrt(int x) {
+        long start = 0;
+        long end = x - 1;
+
+        if(x==0)
+            return 0;
+        if(x==1 || x==2 || x==3)
+            return 1;
+
+        while(start <= end){
+            long mid = start + (end-start)/2;
+
+            if(mid*mid == x)
+                return (int)mid;
+            else if(mid*mid > x)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+
+        return (int)end;
+    }
+
+}
