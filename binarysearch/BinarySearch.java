4bbe2b5b54fb7548311d5e6a14c1d7b2001f21a6
Shivansh-Mittal <shivanshmittal1444@gmail.com>
2022-10-06 08:19:31 +0530

    files added

--- binarysearch/BinarySearch.java
+++ binarysearch/BinarySearch.java
@@ -1,4 +1,6 @@
 package binarysearch;
+
+// leetcode - https://leetcode.com/problems/binary-search/
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {2,4,6,9,99};
@@ -8,7 +10,7 @@ public class BinarySearch {
 
     static int binarySearch(int[] nums, int target){
         int start = 0;
-        int end = nums.length;
+        int end = nums.length - 1;
 
         while(start <= end){
             int mid = start + (end-start)/2;
---
+++ binarysearch/GuessNumber.java
@@ -0,0 +1,36 @@
+package binarysearch;
+
+// leetcode - https://leetcode.com/problems/guess-number-higher-or-lower/
+public class GuessNumber {
+    public static void main(String[] args) {
+        
+    }
+
+    /** 
+     * Forward declaration of guess API.
+     * @param  num   your guess
+     * @return 	     -1 if num is higher than the picked number
+     *			      1 if num is lower than the picked number
+     *               otherwise return 0
+     * int guess(int num);
+    */
+
+    static int guessNumber(int n) {
+        long start = 1;
+        long end = n;
+
+        while(start <= end){
+            long mid = start + (end-start)/2;
+            int res = guess((int)mid);
+
+            if(res == 0)
+                return (int)mid;
+            else if(res == -1)
+                end = mid - 1;
+            else
+                start = mid + 1; 
+        }
+
+        return -1;
+    }
+}
