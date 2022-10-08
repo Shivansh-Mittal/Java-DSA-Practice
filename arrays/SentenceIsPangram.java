c8b17906f89c6f7c43c8350e5858512b3abaf0ff
Shivansh-Mittal <shivanshmittal1444@gmail.com>
2022-10-08 20:54:24 +0530

    arrays solution updated

---
+++ arrays/SentenceIsPangram.java
@@ -0,0 +1,20 @@
+package arrays;
+// leetcode - https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+public class SentenceIsPangram {
+    public static void main(String[] args) {
+        String sentence = "thequickbrownfoxjumpsoverthelazydog";
+        System.out.println(checkIfPangram(sentence));
+    }
+
+    static boolean checkIfPangram(String sentence) {
+        int size  = (sentence.toCharArray()).length;
+        if((sentence.toCharArray()).length < 26)
+            return false;
+        for(char c='a'; c<='z'; c++){
+            if(sentence.indexOf(c) == -1)
+                return false;
+        }
+        return true;
+    }
+
+}
